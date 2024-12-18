ccc52f398e6e432a3a67b80df9373df284da51ea
Hihi12410 <Csillag Marcell@HIHI12410>
2024-12-18 20:34:41 +0100

    feladom xd

--- fileutil.c
+++ fileutil.c
@@ -1,34 +1,37 @@
 #include "fileutil.h"
 
-FILE* openFile(const char* path,const char* mode)
+FILE* openFile(const char* path, const char mode)
 {
 	FILE* file = fopen(path, mode);
-	
-	if (file == NULL) { perror("Couldn't open file(s)!"); handleError(); exit(1); }
+
+	if (file == NULL) { perror("Couldn't open file(s)!"); handleError(); }
 	return &file;
 }
 
 int closeFile(FILE* file)
 {
+	fileCheck(file);
+
 	int res = fclose(file);
 
-	if (res == EOF) { perror("Couldn't close file(s)!"); handleError(); exit(1); }
+	if (res == EOF) { perror("Couldn't close file(s)!"); handleError();}
 	gcAdd(res);
 	return res;
 }
 
 long getFileLen(FILE* file) 
 {
+	fileCheck(file);
 
 	long currLine = ftell(file);
-	if (currLine == EOF) { perror("Error determining pointer of file!"); handleError();exit(1); }
+	if (currLine == EOF) { perror("Error determining pointer of file!"); handleError(); }
 	
-	if (fseek(file, 0, SEEK_END) != 0) { perror("Error seeking EOF!"); handleError(); exit(1); }
+	if (fseek(file, 0, SEEK_END) != 0) { perror("Error seeking EOF!"); handleError(); }
 	
 	long len = ftell(file);
 	
-	if (len < 0) { perror("Error determining length of file(s)!"); exit(1); }
-	if (fseek(file, currLine, SEEK_SET) != 0) { perror("Error restoring file's pointer!"); handleError();exit(1); }
+	if (len < 0) { perror("Error determining length of file(s)!"); ; }
+	if (fseek(file, currLine, SEEK_SET) != 0) { perror("Error restoring file's pointer!"); handleError(); }
 	
 	gcAdd(&currLine);
 	gcAdd(&len);
@@ -37,11 +40,13 @@ long getFileLen(FILE* file)
 
 char* readFile(FILE* file)
 {
+	fileCheck(file);
+
 	long len = getFileLen(file);
 	char *expo = (char *)malloc(len + 1);
 
 	size_t res = fread(expo, sizeof(char), len, file);
-	if (res != sizeof(expo)) { perror("Error reading file(s)!"); handleError(); exit(1); }
+	if (res != sizeof(expo)) { perror("Error reading file(s)!"); handleError(); }
 	expo[len] = '\0';
 	
 	gcAdd(expo);
@@ -51,10 +56,17 @@ char* readFile(FILE* file)
 	return expo;
 }
 
-int writeFile(FILE* file, char* dat, char mode)
+void fileCheck(FILE* file) 
 {
+	if (file == NULL) { perror("Requested file is null!"); handleError(); }
+}
+
+int writeFile(FILE* file, char* dat)
+{
+	fileCheck(file);
+
+	if (fwrite(dat, sizeof(char), strlen(dat), file) != strlen(dat)) { perror("Error writing file(s)!"); handleError(); }
 	
-
-
 	return 0;
+
 }
--- fileutil.h
+++ fileutil.h
@@ -5,13 +5,30 @@
 #include <string.h>
 #include "gc.h"
 
+#ifndef FILEMODES
+#define FILEMODES
 
-FILE* openFile(char* path, char mode);
+#define READ 'r'
+#define WRITE 'w'
+#define APPEND 'a'
+#define READWRITE 'r+'
+#define WRITEREAD 'w+'
+#define APPENDREAD 'a+'
+#define BINREAD 'rb'
+#define BINWRITE 'wb'
+#define BINAPPEND 'ab'
+#define BINREADWRITE 'rb+'
+#define BINWRITEREAD 'wb+'
+#define BINAPPENDREAD 'ab+'
+
+#endif // !FILEMODES
+
+FILE* openFile(const char* path,const char mode);
 int closeFile(FILE* file);
 
 FILE* readFile(FILE* file);
-int writeFile(FILE* file, char* dat, int mode);
+int writeFile(FILE* file, char* dat);
 
-
+void fileCheck(FILE* file);
 
 #endif // !FILEUTIL
